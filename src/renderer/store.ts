d6f9b9faee1d22e969fb14ccb67769813e22be71
airtoxin <airtoxin@icloud.com>
2018-01-24 01:34:55 +0900

    Webpack

--- src/renderer/store.ts
+++ src/renderer/store.ts
@@ -1,16 +1,17 @@
-import { createStore, applyMiddleware, compose, Store } from 'redux';
-import { routerMiddleware } from 'react-router-redux';
+import {createStore, applyMiddleware, compose, Store} from 'redux';
+import {routerMiddleware} from 'react-router-redux';
 import thunk from 'redux-thunk';
-import reducers from './modules';
+import reducers, {GlobalState} from './modules';
 import history from './history';
 
 const router = routerMiddleware(history);
 
 const enhancer = compose(
   applyMiddleware(router, thunk),
-  window['devToolsExtension'] ? window['devToolsExtension']() : () => {},
+  window['devToolsExtension'] ? window['devToolsExtension']() : () => {
+  },
 );
 
-const store = createStore(reducers, enhancer);
+const store = createStore(reducers, enhancer) as Store<GlobalState>;
 
 export default store;
